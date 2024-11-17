d15c8bde786ebd5a2da786be531a215ba5008f06
AdriaTomasMorancho <adriatomasmorancho@gmail.com>
2024-11-17 11:16:08 +0100

    Hecho pegado especial de JSON de la Api SW, también optimización de datos, solo cogemos los datos que queremos

---
+++ StarWars.Infrastructure.Contracts/EntitiesApi/PlanetListSWApiEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using static StarWars.Infrastructure.Contracts.EntitiesApi.PlanetSWApiEntity;
+
+namespace StarWars.Infrastructure.Contracts.EntitiesApi
+{
+    public class PlanetListSWApiEntity
+    {
+        [JsonPropertyName("results")]
+        public List<PlanetSWApiEntity> Data { get; set; } = new List<PlanetSWApiEntity>();
+
+    }
+}
---
+++ StarWars.Infrastructure.Contracts/EntitiesApi/PlanetSWApiEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace StarWars.Infrastructure.Contracts.EntitiesApi
+{
+    public class PlanetSWApiEntity
+    {
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("rotation_period")]
+        public string RotationPeriod { get; set; }
+
+        [JsonPropertyName("orbital_period")]
+        public string OrbitalPeriod { get; set; }
+
+        [JsonPropertyName("climate")]
+        public string Climate { get; set; }
+
+        [JsonPropertyName("population")]
+        public string Population { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+        
+
+    }
+}
